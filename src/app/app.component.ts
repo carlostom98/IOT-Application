b3c78e0a41275a86a88821a6f7bd180d68ecf2b8
FreddyMartinez <fhmartinezq@unal.edu.co>
2020-06-01 15:45:59 -0500

    retrieving data from Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,29 @@
 import { Component } from '@angular/core';
-import {FirebasedbService} from  "./services/firebasedb.service"
+import { FirebasedbService } from './services/firebasedb.service';
+
+interface FbData {
+  Valor: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Primera Aplicacion';
-  variable;
-  minumero: number;
+  title = 'IoT Aplicacion';
+  fbResponse;
+  fbDataList: FbData[];
 
-  constructor(fbservice:FirebasedbService){
-    this.variable=fbservice.infodata().snapshotChanges();
-    console.log(this.variable);
+  constructor(fbservice: FirebasedbService) {
+    this.fbResponse = fbservice
+      .infodata()
+      .valueChanges()
+      .subscribe((res) => {
+        this.fbDataList = res as FbData[];
+        console.log(res);
+        console.log(this.fbDataList);
+      });
+    console.log(this.fbResponse);
   }
 }
